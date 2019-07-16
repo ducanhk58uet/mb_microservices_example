1b90c75d0fb996d91a510300cb2d6f7f1497390d
andy <ducanh54@gmail.com>
2019-07-16 14:14:14 +0700

    config with docker

--- trade-service/src/main/java/com/itlead/trade/TradeServiceApplication.java
+++ trade-service/src/main/java/com/itlead/trade/TradeServiceApplication.java
@@ -2,8 +2,12 @@ package com.itlead.trade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +18,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableOAuth2Client
+@EnableFeignClients
 public class TradeServiceApplication {
 
     public static void main(String[] args) {
---
+++ trade-service/src/main/java/com/itlead/trade/service/package-info.java
@@ -0,0 +1,1 @@
+package com.itlead.trade.service;
