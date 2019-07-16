7ef7b8db35c430dbbbdd1791cb1e13658c65581e
andy <ducanh54@gmail.com>
2019-07-16 16:51:40 +0700

    update account-service

--- auth-service/src/main/java/com/itlead/auth/config/OAuth2AuthorizationConfig.java
+++ auth-service/src/main/java/com/itlead/auth/config/OAuth2AuthorizationConfig.java
@@ -44,17 +44,17 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
                 .withClient("account-service")
                 .secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
                 .authorizedGrantTypes("client_credentials", "refresh_token")
-                .scopes("server")
-                .and()
-                .withClient("statistics-service")
-                .secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
-                .authorizedGrantTypes("client_credentials", "refresh_token")
-                .scopes("server")
-                .and()
-                .withClient("notification-service")
-                .secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
-                .authorizedGrantTypes("client_credentials", "refresh_token")
                 .scopes("server");
+//                .and()
+//                .withClient("statistics-service")
+//                .secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
+//                .authorizedGrantTypes("client_credentials", "refresh_token")
+//                .scopes("server")
+//                .and()
+//                .withClient("notification-service")
+//                .secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
+//                .authorizedGrantTypes("client_credentials", "refresh_token")
+//                .scopes("server");
         // @formatter:on
     }
 
--- hystrix-service/src/main/java/com/itlead/hystrix/HystrixDashboardApplication.java
+++ hystrix-service/src/main/java/com/itlead/hystrix/HystrixDashboardApplication.java
@@ -2,14 +2,14 @@ package com.itlead.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableTurbine
+@EnableTurbineStream
 @EnableHystrixDashboard
-//@EnableCircuitBreaker
+@EnableAsync
 public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
